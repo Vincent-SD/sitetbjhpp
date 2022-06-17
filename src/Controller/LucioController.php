b57cda97ee41878c396ab8a88dfb2efa2b8d2fc7
Tom Gimat <tom6gimat@gmail.com>
2022-06-17 15:11:10 +0200

    giga sécurité de geralt ça fait plaisir

---
+++ src/Controller/LucioController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LucioController extends AbstractController
+{
+    /**
+     * @Route("/lucio", name="app_lucio")
+     */
+    public function index(): Response
+    {
+
+        return $this->render('lucio/index.html.twig', [
+            'controller_name' => 'LucioController',
+        ]);
+    }
+
+    /**
+     * @Route("/lucio-parse", name="lucio_parse")
+     */
+    public function parse() : Response{
+        return $this->render('');
+    }
+}
